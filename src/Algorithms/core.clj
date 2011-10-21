3b316b9c492f7b06f61287711f49321d445b68f3
Phillip Garland <pgarland@gmail.com>
2011-10-21 01:30:59 -0700

    Add docstrings

--- src/Algorithms/core.clj
+++ src/Algorithms/core.clj
@@ -1,15 +1,19 @@
 (ns Algorithms.core)
 
 (defn parent-index [index]
+  "Return the parent index of current node"
   (quot (dec index) 2))
 
 (defn left-index [index]
+  "Return the index of the left node of the current node"
   (* 2 (inc index)))
 
 (defn right-index [index]
+  "Return the index of the left node of the current node"
   (+ (* (inc index) 2) 1))
 
 (defn test-heap
+  "Apply test-fn to every node in the heap"
   ([heap test-fn] (and (test-heap heap (left-index 0) test-fn)
                        (test-heap heap (right-index 0) test-fn)))
   ([heap root-index test-fn] (or (> root-index (count heap))
@@ -19,12 +23,14 @@
 
 
 (defn max-heap? [heap]
+  "Return true if heap is a max heap, false otherwise"
   (test-heap heap
              (fn [heap root-index] (>= (nth heap (parent-index root-index))
                                        (nth heap root-index)))))
 
 
 (defn min-heap? [heap]
+  "Return true if heap is a min heap, false otherwise"
   (test-heap heap
              (fn [heap root-index] (<= (nth heap (parent-index root-index))
                                        (nth heap root-index)))))
