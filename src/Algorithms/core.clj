5cf128d5752fe911dd5837556e0a6465913b56c1
Phillip Garland <pgarland@gmail.com>
2011-10-21 22:03:43 -0700

    max-heapify: new function. Also fix left-index and right-index

--- src/Algorithms/core.clj
+++ src/Algorithms/core.clj
@@ -6,11 +6,11 @@
 
 (defn left-index [index]
   "Return the index of the left node of the current node"
-  (* 2 (inc index)))
+  (inc (* 2 index)))
 
 (defn right-index [index]
   "Return the index of the left node of the current node"
-  (+ (* (inc index) 2) 1))
+  (* (inc index) 2))
 
 (defn test-heap
   "Apply test-fn to every node in the heap"
@@ -34,3 +34,19 @@
   (test-heap heap
              (fn [heap root-index] (<= (nth heap (parent-index root-index))
                                        (nth heap root-index)))))
+
+(defn swap [a i j]
+  "Return a vector where the values in indices i and j have been swapped"
+  (assoc a i (a j) j (a i)))
+
+(defn max-heapify [array root]
+  (let [left (left-index root)
+        right (right-index root)
+        valid-indices (filter #(< % (count array))
+                              [root left right])
+        max-index (apply (partial max-key array) valid-indices)]
+    (print root valid-indices max-index)
+    (if (not (= max-index root))
+      (max-heapify (swap array root max-index) max-index)
+      array)))
+        
