a9338b5ff87c7c92cbffe023d21ab599a585828b
Phillip Garland <pgarland@gmail.com>
2011-10-28 21:25:31 -0700

    Bump version number

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject algorithms "0.0.2"
+(defproject algorithms "0.0.3"
   :description "Various algorithms and data structures, implemented in clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]])
