5992daddf6b3c311fbb085ef9e53003ae51ec2ff
Phillip Garland <pgarland@gmail.com>
2011-10-29 00:19:59 -0700

    Move PriorityQueue code from core.clj into new file PriorityQueue.clj

---
+++ src/Algorithms/PriorityQueue.clj
@@ -0,0 +1,34 @@
+(ns Algorithms.PriorityQueue
+  (:use [Algorithms.heap]))
+
+(defprotocol PriorityQueue
+  (insert [Q val])
+  (peek [Q])
+  (pop [Q])
+  (set-val [Q idx val]))
+
+(defrecord MaxPriorityQueue [heap])
+
+(extend-type MaxPriorityQueue
+  PriorityQueue
+  (insert [Q val]
+    (MaxPriorityQueue. (Maxheap-insert (:heap Q val))))
+  (peek [Q]
+    (heap-max (:heap Q)))
+  (pop [Q]
+    (MaxPriorityQueue. (heap-extract-max (:heap Q))))
+  (set-val [Q idx val]
+    (MaxPriorityQueue. (heap-increase-key (:heap Q) idx val))))
+
+(defrecord MinPriorityQueue [heap])
+
+(extend-type MinPriorityQueue
+  PriorityQueue
+  (insert [Q val]
+    (MinPriorityQueue. (Minheap-insert (:heap Q val))))
+  (peek [Q]
+    (heap-min (:heap Q)))
+  (pop [Q]
+    (MinPriorityQueue. (heap-extract-min (:heap Q))))
+  (set-val [Q idx val]
+    (MinPriorityQueue. (heap-decrease-key (:heap Q) idx val))))
--- src/Algorithms/core.clj
+++ src/Algorithms/core.clj
@@ -5,34 +5,3 @@
 ;; Cormen TH, Leiserson CE, Rivest, RL, and Stein C
 ;; http://mitpress.mit.edu/algorithms/
 
-(defprotocol PriorityQueue
-  (insert [Q val])
-  (peek [Q])
-  (pop [Q])
-  (set-val [Q idx val]))
-
-(defrecord MaxPriorityQueue [heap])
-
-(extend-type MaxPriorityQueue
-  PriorityQueue
-  (insert [Q val]
-    (MaxPriorityQueue. (Maxheap-insert (:heap Q val))))
-  (peek [Q]
-    (heap-max (:heap Q)))
-  (pop [Q]
-    (MaxPriorityQueue. (heap-extract-max (:heap Q))))
-  (set-val [Q idx val]
-    (MaxPriorityQueue. (heap-increase-key (:heap Q) idx val))))
-
-(defrecord MinPriorityQueue [heap])
-
-(extend-type MinPriorityQueue
-  PriorityQueue
-  (insert [Q val]
-    (MinPriorityQueue. (Minheap-insert (:heap Q val))))
-  (peek [Q]
-    (heap-min (:heap Q)))
-  (pop [Q]
-    (MinPriorityQueue. (heap-extract-min (:heap Q))))
-  (set-val [Q idx val]
-    (MinPriorityQueue. (heap-decrease-key (:heap Q) idx val))))
