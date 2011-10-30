510c011ff331534165aa4f1d374575358348bc32
Phillip Garland <pgarland@gmail.com>
2011-10-29 22:29:45 -0700

    Add docstrings

--- src/Algorithms/BinarySearchTree.clj
+++ src/Algorithms/BinarySearchTree.clj
@@ -3,27 +3,32 @@
 (defrecord BinarySearchTree [val left right])
 
 (defn walk-tree-inorder [t node-fn]
+  "Visit each node of the tree in order, calling node-fn on each"
   (when t
     (do (walk-tree-inorder (:left t) node-fn)
         (node-fn t)
         (walk-tree-inorder (:right t) node-fn))))
 
 (defn walk-tree-post-order [t node-fn]
+    "Visit each node of the tree in preorder, calling node-fn on each"
   (when t
     (do (node-fn t)
         (walk-tree-preorder (:left t) node-fn)
         (walk-tree-preorder (:right t) node-fn))))
 
 (defn walk-tree-postorder [t node-fn]
+      "Visit each node of the tree in postorder, calling node-fn on each"
   (when t
     (do (walk-tree-postorder (:left t) node-fn)
         (walk-tree-postorder (:right t) node-fn)
         (node-fn t))))
 
 (defn print-tree [t walk-fn]
+  "Print each value in the tree, with the order determined by walk-fn"
   (walk-fn t #(println (:val %))))
 
 (defn search [t val]
+  "Return the node that contains val"
   (if (or (= t nil) (= (:val t) val))
     (loop [tree t]
       (let [branch (if (< (:val tree) val)
