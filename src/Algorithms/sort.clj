c9c7974aabbeb0f4c8dae6f66bf982222bb0a162
Phillip Garland <pgarland@gmail.com>
2011-11-14 01:48:52 -0800

    Add insertion sort

--- src/Algorithms/sort.clj
+++ src/Algorithms/sort.clj
@@ -1,2 +1,14 @@
 (ns Algorithms.sort)
 
+(defn insertion-sort
+  ([a] (insertion-sort [] a))
+  ([l r] (if (not (seq r))
+           l
+           (recur (insert (first r) l) (rest r)))))
+
+(defn insert
+  ([elt a] (insert elt [] a))
+  ([elt l r]) (if (or (not (seq r))
+                      (<= elt (first r)))
+                (reduce into [l [elt] r])
+                  
