984118f9e1872d6c2005f81c8ff69ecb830c756d
Phillip Garland <pgarland@gmail.com>
2012-12-20 18:04:20 -0800

    Update to clojure 1.4 and add user profile

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject algorithms "0.0.4"
   :description "Various algorithms and data structures, implemented in clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:user {}})
